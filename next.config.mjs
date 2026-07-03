ba54649aaad55f3108852031db6d8ea39ebcaa8a
christineshammi244 <christineshammi244@github.com>
2026-07-04 00:31:24 +0300

    rm package-lock.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,32 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'res.cloudinary.com',
-            pathname: '/**',
-          },
-          
-          
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '://cloudinary.com',
+        pathname: '/**',
       },
-      exoperimental: {
-        serverActions: {
-        bodySizeLimit: '10mb',
-      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '10mb',
     },
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-  };
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  env: {
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_OUT_URL: "/",
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: "/admin",
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL: "/admin",
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: "/m/login",
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: "/m/login"
+  }
+};
+
 export default nextConfig;
